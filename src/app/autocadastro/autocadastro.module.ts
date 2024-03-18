7c05437f3b57c51d3eca6255875ade76f3267d76
andre-alex <aalexjankoski@gmail.com>
2024-03-18 14:45:54 -0300

    initial

---
+++ src/app/autocadastro/autocadastro.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AutocadastroComponent } from './autocadastro/autocadastro.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    AutocadastroComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ]
+})
+export class AutocadastroModule { }
---
+++ src/app/autocadastro/autocadastro/autocadastro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-autocadastro',
+  templateUrl: './autocadastro.component.html',
+  styleUrl: './autocadastro.component.css'
+})
+export class AutocadastroComponent {
+
+}
