10b01f49dc38a88276ee210f7f101ed1e433cdb3
andre-alex <aalexjankoski@gmail.com>
2024-03-18 14:42:52 -0300

    initial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AutocadastroComponent } from './autocadastro/autocadastro/autocadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'autocadastro', pathMatch:'full'},
+  {path:'autocadastro', component: AutocadastroComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
