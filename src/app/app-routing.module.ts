4b1bc360f18c2b38162d9093c524f116dbaa89e3
JV <joaovitoraraujo1821@gmail.com>
2024-04-22 15:47:43 -0300

    Deposito, Saque, Transferencia

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { AutocadastroComponent } from './autocadastro/autocadastro/autocadastro.component';
 import { LoginComponent } from './login/login/login.component';
 import { ClientesComponent, InserirEditarGerenteComponent, ListarGerenteComponent, TelaInicialComponent } from './administrador';
-import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { SaqueComponent } from './saque/saque.component';
 import { TransferenciaComponent } from './transferencia/transferencia.component';
+import { DepositoComponent } from './deposito/deposito.component';
 
 
 const routes: Routes = [
   {path:'', redirectTo:'login', pathMatch:'full'},
   {path:'autocadastro', component: AutocadastroComponent },
   {path:'login', component:  LoginComponent},
-  {path: 'home', component: HomeComponent},
+  {path: 'home', component: DashboardComponent},
   {path: 'saque', component: SaqueComponent},
   {path: 'transferencia', component: TransferenciaComponent},
+  {path: 'deposito', component: DepositoComponent},
 
 
   //Administrador
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
 import { AutocadastroModule } from './autocadastro/autocadastro.module';
 import { LoginModule } from './login/login.module';
 import { SharedModule } from './shared/shared.module';
 import { AdministradorModule } from './administrador/administrador.module';
-import { provideNgxMask } from 'ngx-mask';
-import { HomeComponent } from './home/home.component';
+import { provideNgxMask, NgxMaskDirective } from 'ngx-mask';
+import { DashboardComponent} from './dashboard/dashboard.component';
 import { SaqueComponent } from './saque/saque.component';
 import { TransferenciaComponent } from './transferencia/transferencia.component';
 import { DepositoComponent } from './deposito/deposito.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+    DashboardComponent,
     SaqueComponent,
     TransferenciaComponent,
     DepositoComponent,
@@ -25,13 +26,16 @@ import { DepositoComponent } from './deposito/deposito.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     LoginModule,
     AutocadastroModule,
     FormsModule,
     AutocadastroModule,
     SharedModule,
-    AdministradorModule
+    NgxMaskDirective,
+    AdministradorModule,
+  
   ],
 
   providers: [
