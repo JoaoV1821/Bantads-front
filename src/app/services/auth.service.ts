bd70f92b5c8abea9767061f3306cbe3eba975390
JV <joaovitoraraujo1821@gmail.com>
2024-07-15 13:39:42 -0300

    Add auth guard

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isAuthenticated = false; // You can set this based on your authentication logic
+
+  constructor() {}
+
+  // Example: Check if the user is authenticated
+  checkAuthentication(): boolean {
+     return this.isAuthenticated;
+  }
+
+  login(): void {
+    this.isAuthenticated = true;
+  }
+}
